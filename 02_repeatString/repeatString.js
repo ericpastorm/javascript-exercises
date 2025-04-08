4cca0277b39ca377ab40be5832ab9b481d3e16e1
eric <38719351+ericpastorm@users.noreply.github.com>
2025-04-08 23:32:22 +0200

    Exercise 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let globstring = "";
+    if(num < 0){
+        globstring = "ERROR"
+    } else {
+        for (let i = 0; i < num; i++) {;
+            globstring += string;
+            }
+    }
+return globstring;
 };
 
 // Do not edit below this line
