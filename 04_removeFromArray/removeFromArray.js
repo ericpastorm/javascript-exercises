45048f41b94d272e6b57f4d6d787efd32e237995
eric <38719351+ericpastorm@users.noreply.github.com>
2025-04-09 23:22:57 +0200

    Solution to exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    for (let i = 0; i < array.length; i++) {
+        if(remove.length > 1){
+            for (let z = 0; z < remove.length; z++) {
+                if(array[i] === remove[z]){
+                    array.splice(i, 1);
+                    i+=-1;
+                }
+            }
+        } else {
+            if(array[i] === remove[0]){
+                array.splice(i, 1);
+                i+=-1;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
