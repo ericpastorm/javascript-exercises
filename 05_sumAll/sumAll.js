39dc3e8b7cd2c0c1daa749917a662fd260c159c4
eric <38719351+ericpastorm@users.noreply.github.com>
2025-04-09 23:57:26 +0200

    Solution to exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+let suma = 0;
+if(num1 < 0 || num2 < 0) return "ERROR";
+if(!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+if(num1 < num2){
+    for (let i = num1; i <= num2; i++) {
+        suma+=i;
+    }
+} else {
+    for (let i = num2; i <= num1; i++) {
+        suma+=i;
+    }
+}
+return suma;
 };
 
 // Do not edit below this line
