54777303a29258fc610a9b87a244afcbfba12795
eric <38719351+ericpastorm@users.noreply.github.com>
2025-04-10 01:17:00 +0200

    Solution to exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let lastDigits = "";
+    if(year.toString().length > 2){
+        lastDigits += year.toString().slice(year.toString().length-2);
+    }
+    if(lastDigits == "00"){
+        if(year%400 == 0 && year%4 == 0) return true;
+    } else {
+        if(year%4 == 0) return true;
+    }
+    return false;
 };
-
 // Do not edit below this line
 module.exports = leapYears;
