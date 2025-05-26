cb3c8e2ec1e268ea422ae8ee9ddd926bf00c86e7
eric <38719351+ericpastorm@users.noreply.github.com>
2025-05-26 18:41:46 +0200

    Updated the hub and added solution to exercises

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,18 +1,15 @@
 const sumAll = function(num1, num2) {
 let suma = 0;
-if(num1 < 0 || num2 < 0) return "ERROR";
-if(!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
-if(num1 < num2){
-    for (let i = num1; i <= num2; i++) {
-        suma+=i;
-    }
-} else {
-    for (let i = num2; i <= num1; i++) {
-        suma+=i;
-    }
+if(!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0){
+    return "ERROR";
+}
+const start = Math.min(num1, num2);
+const end = Math.max(num1, num2);
+for (let i = start; i <= end; i++) {
+    suma+= i;
 }
 return suma;
 };
-
+console.log(sumAll(1,4))
 // Do not edit below this line
 module.exports = sumAll;
